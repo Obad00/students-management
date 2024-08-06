968c83813d10ced0a8a1dae473b07fcb0ad2686e
Adama Dabo <dabodabo9064@gmail.com>
2024-08-06 16:20:04 +0000

    Je faisais la restriction des routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
@@ -15,18 +16,7 @@ class AuthController extends Controller
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        // Récupérer l'utilisateur authentifié
-        $user = JWTAuth::parseToken()->authenticate();
-
-        // Retourner le token ainsi que les informations de l'utilisateur
-        return response()->json([
-            'token' => $token,
-            'user' => [
-                'name' => $user->name,
-                'prenom' => $user->prenom,
-                'email' => $user->email,
-            ]
-        ]);
+        return response()->json(compact('token'));
     }
 
     public function logout()
